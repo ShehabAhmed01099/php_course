f3f8b3be9ea1fffe04860f68ee043dc724311e13
ShehabAhmed01099 <91156504+ShehabAhmed01099@users.noreply.github.com>
2023-08-17 06:14:01 -0700

    Add files via upload

---
+++ embed.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php  
+            $title = "Hello";
+        ?>
+        
+        <h1><?php
+            // echo function is used to output the php page in local server
+            echo $title 
+        ?></h1>
+    </body>
+</html>
